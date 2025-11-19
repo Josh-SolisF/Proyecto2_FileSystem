26175d74edc4fc70b20397c7047c6383f5269638
Josh-SolisF <josh.solisf@gmail.com>
2025-11-19 14:47:30 -0600

    endif at the end of bitmaps.g

--- bitmaps.h
+++ bitmaps.h
@@ -1,5 +1,6 @@
 #ifndef BITMAPS_H
 #define BITMAPS_H
+
 #include "fs_basic.h"
 
 int  allocate_inode(void);
@@ -7,3 +8,4 @@ void free_inode(int inode_id);
 int  allocate_block(void);
 void free_block(int block_num);
 
+#endif
