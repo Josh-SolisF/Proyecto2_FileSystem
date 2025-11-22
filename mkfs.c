7c179ef0f7f099025d871c144438cfdcb97e790d
Josh-SolisF <josh.solisf@gmail.com>
2025-11-22 00:10:47 -0600

    added parameter to rename

--- mkfs.c
+++ mkfs.c
@@ -11,8 +11,8 @@
 #include <errno.h>
 
 int mkfs(int argc, char **argv) {
-    const char *folder = (argc >= 2) ? argv[1] : "./qrfolder";
-    u32 block_size   = 1024;
+const char *folder = (argc >= 3) ? argv[2] : "./qrfolder";
+u32 block_size   = 1024;
     u32 total_blocks = DEFAULT_TOTAL_BLOCKS;  // <=128
     u32 total_inodes = DEFAULT_TOTAL_INODES;  // <=128
 
